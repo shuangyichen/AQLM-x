b0976759a2dfbad5aecefbf970cc9cd067b8158e
liza-babaoglu <67078190+liza-babaoglu@users.noreply.github.com>
2025-09-30 11:57:02 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,41 @@
-#!/bin/sh
-#SBATCH --job-name=eval
-#SBATCH --gres=gpu:rtx6000:1
-#SBATCH --qos=normal
-#SBATCH --time=10:00:00
-#SBATCH -c 30
-#SBATCH --mem=60G
-#SBATCH --output=slurm-%j.out
-#SBATCH --error=slurm-%j.err
+# run the following in terminal:
+sbatch \
+  --account=aip-khisti \
+  --nodes=1 \
+  --gres=gpu:l40s:4\
+  --ntasks-per-node=1 \
+  --mem=120G \
+  --cpus-per-task=4 \
+  --time=40:00:00 \
+  quantize.sh
 
 
-export CUDA_HOME=/pkgs/cuda-12.4
-export PATH=$CUDA_HOME/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-# module load cuda-12.4
+
+
+#no longer needed in the new Killarney setup.
+# #!/bin/sh
+# #SBATCH --job-name=eval
+# #SBATCH --gres=gpu:rtx6000:1
+# #SBATCH --qos=normal
+# #SBATCH --time=10:00:00
+# #SBATCH -c 30
+# #SBATCH --mem=60G
+# #SBATCH --output=slurm-%j.out
+# #SBATCH --error=slurm-%j.err
+
 
 # export CUDA_HOME=/pkgs/cuda-12.4
 # export PATH=$CUDA_HOME/bin:$PATH
 # export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
-. /scratch/ssd004/scratch/chensy/envs/dora_llama
-# export CUDA_HOME=/pkgs/cuda-12.1
-# export PATH=$CUDA_HOME/bin:$PATH
-# export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+# # module load cuda-12.4
 
-lsmod | grep -i nvidia
-sh quantize.sh
+# # export CUDA_HOME=/pkgs/cuda-12.4
+# # export PATH=$CUDA_HOME/bin:$PATH
+# # export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+# . /scratch/ssd004/scratch/chensy/envs/dora_llama
+# # export CUDA_HOME=/pkgs/cuda-12.1
+# # export PATH=$CUDA_HOME/bin:$PATH
+# # export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+# lsmod | grep -i nvidia
+# sh quantize.sh
