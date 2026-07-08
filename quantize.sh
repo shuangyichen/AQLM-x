d5bc8a2c3728f51fcac87e32a011464288d56092
liza-babaoglu <67078190+liza-babaoglu@users.noreply.github.com>
2026-07-08 12:34:21 -0400

    Updated quantize.sh

--- quantize.sh
+++ quantize.sh
@@ -1,11 +1,9 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3   
-export MODEL_PATH=/model-weights/Llama-2-7b-hf/    # /scratch/ssd004/scratch/chensy/hf_home/models--meta-llama--Llama-2-7b-hf/blobs/2ef41cbc275000b29afe157ba487f0530b8c26dc
+export MODEL_PATH=/model-weights/Llama-2-7b-hf/    
 export DATASET_PATH=pajama
 export SAVE_PATH=/scratch/ssd004/scratch/babaogl4/5x8-Llama7b-OG/
-# export WANDB_PROJECT=MY_AQ_EXPS
-# export WANDB_NAME=COOL_EXP_NAME
 
-# liz: increase the number of samples from 1024 to ...
+
 python main.py $MODEL_PATH $DATASET_PATH \
  --nsamples=1024 \
  --val_size=32 \
@@ -35,4 +33,4 @@ python main.py $MODEL_PATH $DATASET_PATH \
 #  --finetune_keep_best \
 #  --local_batch_size=1 \
 #  --offload_activations \
-#  --load /scratch/ssd004/scratch/babaogl4/5x8-Llama7b-Mat/
+#  --load $SAVE_PATH
